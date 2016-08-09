896711eb91ff0cc66386447f49f2d81b86cd7435
garfeng <garfeng_gu@163.com>
2016-08-09 19:36:26 +0800

    修改事件合成方式预加载

--- pre_load_event.rb
+++ pre_load_event.rb
@@ -4,7 +4,7 @@
 # 事件的合成属性预加载
 # 比如增加一个加法的事件，在进入地图时即已经是加法
 # 使用方法：
-# 添加事件，在名称里添加 ：bm1
+# 添加事件，在名称里添加 ：bm1 或者 bm2 ，其中，bm1为加法，bm2为减法
 #----------------------------------------------------------------------------
 
 class Game_Event < Game_Character
@@ -15,9 +15,10 @@ class Game_Event < Game_Character
   alias back_setup_page_settings setup_page_settings
   def setup_page_settings
     back_setup_page_settings
-    if self.instance_variable_get(:@event).name =~ /bm1/
+    if self.instance_variable_get(:@event).name =~ /bm([1|2])/
       #p self.instance_variable_get(:@event).name
-      @blend_type = 1
+      @blend_type = $1.to_i
     end
+
   end
 end
